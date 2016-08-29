56639a490c17685b6a5ae467cf4cde9f55321c2e
Vladimir Yartsev <vovayartsev@gmail.com>
2016-08-29 11:50:16 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+```bash
+curl -H "Content-Type: application/json" -X POST -d '{"name":"xyz","species":"cat"}' http://localhost:3000/pets
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+var _ = require('koa-route');
+var koa = require('koa');
+var parse = require('co-body');
+var app = koa();
+
+var db = {
+  tobi: { name: 'tobi', species: 'ferret' },
+  loki: { name: 'loki', species: 'ferret' },
+  jane: { name: 'jane', species: 'ferret' }
+};
+
+var pets = {
+  list: function *(){
+    var names = Object.keys(db);
+    this.body = JSON.stringify(names)
+  },
+
+  show: function *(name){
+    var pet = db[name];
+    if (!pet) return this.throw('cannot find that pet', 404);
+    this.body = JSON.stringify(pet);
+  },
+
+  create: function *() {
+    var body = yield parse(this);
+    db[body.name] = body;
+    this.body = body;
+  }
+};
+
+app.use(_.get('/pets', pets.list));
+app.use(_.post('/pets', pets.create));
+app.use(_.get('/pets/:name', pets.show));
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('listening on port ' + port);
